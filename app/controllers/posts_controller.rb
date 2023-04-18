853f005320392b353e33592751a3f8f52057b8fa
TECHkoumu <nariyuki08@gmail.com>
2023-04-18 22:06:12 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,10 @@ class PostsController < ApplicationController
   #end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    #binding.pry
+    post = Post.create(content: params[:content])
+    render json:{post: post}
+    #render json:{キー:オブジェクトの値}→json形式でキーと値のセットをJSに送信
+    #部分テンプレートでのrenderと違う。コントローラーに書く場合はレスポンスの生成
   end
 end
