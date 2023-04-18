0e58cb13bd88243bd98c7a435238690c48db1588
TECHkoumu <nariyuki08@gmail.com>
2023-04-18 21:27:54 +0900

    コントローラーにリクエストを送信する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,19 @@
 function post(){
-  console.log("イベント発火");
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click",(e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
+    //formというidを持つ要素を取得
+    const formData = new FormData(form);
+    // フォーム入力された値を取得
+    const XHR = new XMLHttpRequest();
+    //XHRという変数にXMLH~~オブジェクトを生成して格納
+    XHR.open("POST","/posts",true);
+    //openでリクエストの内容を指定。("HTTPメソッド","パス","非同期のONOFF")
+    XHR.responseType = "json";
+    //データフォーマットをjson形式で返してね、ということ
+    XHR.send(formData);
+  });
 };
 
 window.addEventListener('load',post);
